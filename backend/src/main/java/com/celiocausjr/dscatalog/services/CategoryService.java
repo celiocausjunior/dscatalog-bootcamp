9682eac7b11967b5d77a0ffcfe549ea3f5a09a0f
Celio Caus Junior <celiocausjunior@gmail.com>
2020-10-05 15:35:41 -0300

    Find by Id, Exception Handler Resource

--- backend/src/main/java/com/celiocausjr/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/celiocausjr/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.celiocausjr.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.celiocausjr.dscatalog.dto.CategoryDTO;
 import com.celiocausjr.dscatalog.entities.Category;
 import com.celiocausjr.dscatalog.repositories.CategoryRepository;
+import com.celiocausjr.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,4 +24,12 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not Found"));
+		return new CategoryDTO(entity);
+	}
+
 }
