671ec84f156036dc113d25f269e78a37720bd6c3
Celio Caus Junior <celiocausjunior@gmail.com>
2020-10-17 13:31:50 -0300

    UseEffect and UseState

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+
 
 const App = () => {
+    const [counter, setCounter] = useState(0);
+
+    useEffect(() => {
+        console.log("componente inicializado")
+    }, []);
+
     return (
         <div className="container mt-5">
-            <Alert text = "Celio" />
-            <Alert text = "Bootcamp" />
+           <button className="btn btn-primary mr-5"
+           onClick={() => setCounter(counter + 1)}
+           >
+               +               
+           </button>
+           <span>
+               {counter}
+           </span>
+           <button className="btn btn-primary ml-5"
+           onClick={() => setCounter(counter -1)}
+           >
+            -
+            </button>
+            {counter > 5 && <h1>O valor é maior que 5</h1>}
+            {counter < 0 && <h1>O valor é menor que 0</h1>}
         </div>
     );
 }
