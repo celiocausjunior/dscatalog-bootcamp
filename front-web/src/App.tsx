9a01ce8b6f0454bb475155e009bbff1711c6806e
Celio Caus Junior <celiocausjunior@gmail.com>
2020-10-18 16:07:35 -0300

    Initial front-web application

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,31 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, {  } from 'react';
 
 
 const App = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        console.log("componente inicializado")
-    }, []);
-
-    return (
-        <div className="container mt-5">
-           <button className="btn btn-primary mr-5"
-           onClick={() => setCounter(counter + 1)}
-           >
-               +               
-           </button>
-           <span>
-               {counter}
-           </span>
-           <button className="btn btn-primary ml-5"
-           onClick={() => setCounter(counter -1)}
-           >
-            -
-            </button>
-            {counter > 5 && <h1>O valor é maior que 5</h1>}
-            {counter < 0 && <h1>O valor é menor que 0</h1>}
-        </div>
+    return(
+    <h1>App</h1>
     );
 }
 
