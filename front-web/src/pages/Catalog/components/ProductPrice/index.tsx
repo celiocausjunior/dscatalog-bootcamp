96522cf7a4757af5526a3103aaa96011d4242c3e
Celio Caus Junior <celiocausjunior@gmail.com>
2020-11-01 21:11:21 -0300

    Currency format

--- front-web/src/pages/Catalog/components/ProductPrice/index.tsx
+++ front-web/src/pages/Catalog/components/ProductPrice/index.tsx
@@ -6,11 +6,16 @@ type Props ={
 }
 
 
+const formatPrice = (price: number) => {
+    return new Intl.NumberFormat('pt-BR', {minimumFractionDigits: 2}).format(price);
+}
+
+
 const ProductPrice = ({ price }:Props) => (
     <div className="product-price-container">
     <span className="product-currency">R$</span>
     <h3 className="product-price">
-    {price}
+    {formatPrice(price)}
     </h3>
 </div>
 );
