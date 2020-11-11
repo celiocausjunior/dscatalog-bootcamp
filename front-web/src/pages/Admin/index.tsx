6d95a7fe8820f5f25b25365b4b32463115c00e0a
Celio Caus Junior <celiocausjunior@gmail.com>
2020-11-10 22:25:08 -0300

    Admin Routes

--- front-web/src/pages/Admin/index.tsx
+++ front-web/src/pages/Admin/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import './styles.scss';
 
 const Admin = () => (
+    <div className="admin-container">
     <Navbar/>
+    <div className="admin-content">
+        <Switch>
+            <Route path="/admin/products">
+                <h1>products</h1>
+            </Route>
+
+            <Route path="/admin/categories">
+                <h1>categories</h1>
+            </Route>
+
+            <Route path="/admin/users">
+                <h1>users</h1>
+            </Route>
+        </Switch>
+      </div>
+    </div>
     );
 
 export default Admin;
