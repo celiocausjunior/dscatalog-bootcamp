bf91a03d6a8fbf5bd96e3c4166fe741d4d44e03b
Celio Caus Junior <celiocausjunior@gmail.com>
2020-11-11 18:17:41 -0300

    Base Form and Product Form

--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import List from './List';
+import Form from './Form';
 
 const Products = () => {
     return (
         <div>
-            <Link to="/admin/products" className="mr-5">
-                Listar Produtos
-            </Link>
-            <Link to="/admin/products/create" className="mr-5">
-                Criar Produtos
-            </Link>
-            <Link to="/admin/products/10" className="mr-5">
-                Editar Produtos
-            </Link>
             <Switch>
                 <Route path="/admin/products" exact>
-                    <h1>Lista de Produtos</h1>
+                    <List/>
                 </Route>
                 <Route path="/admin/products/create">
-                    <h1>Criar Novo Produtos</h1>
+                    <Form/>
                 </Route>
                 <Route path="/admin/products/:productId">
                     <h1>Editar Produto</h1>
