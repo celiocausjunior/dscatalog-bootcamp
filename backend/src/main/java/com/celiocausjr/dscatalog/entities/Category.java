3b4131552b262ec7042c0e02d7b6a8a2fdb15880
Celio Caus Junior <celiocausjunior@gmail.com>
2020-11-19 16:24:10 -0300

    User, Role and Database Seeding

--- backend/src/main/java/com/celiocausjr/dscatalog/entities/Category.java
+++ backend/src/main/java/com/celiocausjr/dscatalog/entities/Category.java
@@ -2,12 +2,15 @@ package com.celiocausjr.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -28,6 +31,9 @@ public class Category implements Serializable {
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -61,12 +67,16 @@ public class Category implements Serializable {
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
-	
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@PrePersist
 	public void prePersist() {
 		createdAt = Instant.now();
 	}
-	
+
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
