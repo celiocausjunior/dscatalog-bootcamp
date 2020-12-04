4ea87c4c69d0f928a07524702b91f4137c8ac60c
Celio Caus Junior <celiocausjunior@gmail.com>
2020-12-03 21:48:44 -0300

    Update validator correction

--- backend/src/main/java/com/celiocausjr/dscatalog/entities/User.java
+++ backend/src/main/java/com/celiocausjr/dscatalog/entities/User.java
@@ -31,7 +31,7 @@ public class User implements UserDetails, Serializable {
 	private Long id;
 	private String firstName;
 	private String lastName;
-
+	
 	@Column(unique = true)
 	private String email;
 	private String password;
