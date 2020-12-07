00ea354d89a273a6c831789659e9c7924c3bd86c
Celio Caus Junior <celiocausjunior@gmail.com>
2020-12-07 14:45:01 -0300

    Auth-Login Admin Page

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,4 +1,6 @@
+import ButtonIcon from 'core/components/ButtonIcon';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AuthCard from '../AuthCard';
 import './styles.scss';
 
@@ -6,7 +8,23 @@ const Login = () => {
     return (
         <AuthCard title="Login" >
             <form className="login-form">
-                <h1>Formulário de Login</h1>
+                <input type="email" className="form-control input-base margin-bottom-30"
+                    placeholder="Email"
+                />
+                <input type="password" className="form-control input-base "
+                    placeholder="Password"
+                />
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha?
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="Logar" />
+                </div>
+                <div className="text-center">
+                    <span className="not-registered"> Não tem cadastro? </span>
+                    <Link to="/admin/auth/register" className="login-link-register"> CADASTRAR
+                </Link>
+                </div>
             </form>
         </AuthCard>
     );
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.scss'
-import { ReactComponent as MainImage} from 'core/assets/images/main-image.svg'
+import { ReactComponent as MainImage } from 'core/assets/images/main-image.svg'
 import ButtonIcon from 'core/components/ButtonIcon';
 import { Link } from 'react-router-dom'
 
@@ -8,16 +8,18 @@ const Home = () => (
     <div className="home-container">
         <div className="row home-content card-base border-radius-20">
             <div className="col-6">
-            <h1 className = "text-title">Conheça o melhor catálogo de produtos</h1>
-            <p className = "text-subtitle">
-                Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.
+                <h1 className="text-title">Conheça o melhor catálogo de produtos</h1>
+                <p className="text-subtitle">
+                    Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.
             </p>
-            <Link to="/products">
-                <ButtonIcon text="Inicie agora a sua busca"/>
-            </Link>
+                <div className="button-home">
+                    <Link to="/products">
+                        <ButtonIcon text="Inicie agora a sua busca" />
+                    </Link>
+                </div>
             </div>
             <div className="col-6">
-                <MainImage className="main-image"/>
+                <MainImage className="main-image" />
             </div>
         </div>
     </div>
