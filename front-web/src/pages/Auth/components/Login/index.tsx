9491105604c314c3c834419016991ba5587b1449
Celio Caus Junior <celiocausjunior@gmail.com>
2020-12-08 18:36:50 -0300

    Warnings e Alerts Login

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,5 +1,5 @@
 import ButtonIcon from 'core/components/ButtonIcon';
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AuthCard from '../AuthCard';
 import { useForm } from 'react-hook-form';
@@ -8,29 +8,42 @@ import { makeLogin } from 'core/utils/request';
 
 
 type FormData = {
-    username:string;
-    password:string;
+    username: string;
+    password: string;
 }
 
 const Login = () => {
     const { register, handleSubmit } = useForm<FormData>();
 
+    const [hasError, setHasError] = useState(false);
+
     const onSubmit = (data: FormData) => {
         console.log(data);
-        makeLogin(data);
+        makeLogin(data)
+            .then(response => {
+                setHasError(false);
+            })
+            .catch(() => {
+                setHasError(true);
+            })
     }
     return (
         <AuthCard title="Login" >
+            {hasError && (
+                <div className="alert alert-danger mt-5 text-center">
+                    Usuário ou senha inválidos
+                </div>
+            )}
             <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
                 <input type="email" className="form-control input-base margin-bottom-30"
                     placeholder="Email"
-                    name="username" 
-                    ref={register}
+                    name="username"
+                    ref={register({ required: true })}
                 />
                 <input type="password" className="form-control input-base "
                     placeholder="Senha"
-                    name="password" 
-                    ref={register}
+                    name="password"
+                    ref={register({ required: true })}
                 />
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha?
