99f99dcd44f21ef2fd0c2cc08c4c28dd36a505b8
Celio Caus Junior <celiocausjunior@gmail.com>
2020-12-16 17:13:01 -0300

    Add Logout at Navbar

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -2,36 +2,52 @@ import React from 'react';
 import './styles.scss';
 import { Link, NavLink } from 'react-router-dom';
 
-const Navbar = () => (
-    <nav className="row bg-primary main-nav">
-        <div className="col-3">
-            <Link to="/" className="nav-logo-text">
-                <h4>DS Catalog</h4>
-            </Link>
-        </div>
-        <div className="col-6">
-            <ul className="main-menu">
-                <li>
-                    <NavLink to="/" activeClassName="active" exact>
-                        HOME
+const Navbar = () => {
+    const currentUser = "email";
+
+    return (
+        <nav className="row bg-primary main-nav">
+            <div className="col-3">
+                <Link to="/" className="nav-logo-text">
+                    <h4>DS Catalog</h4>
+                </Link>
+            </div>
+            <div className="col-6">
+                <ul className="main-menu">
+                    <li>
+                        <NavLink to="/" activeClassName="active" exact className="nav-link">
+                            HOME
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/products" activeClassName="active">
-                        CATÁLOGO
+                    </li>
+                    <li>
+                        <NavLink to="/products" activeClassName=" active" className="nav-link">
+                            CATÁLOGO
                     </NavLink>
-                </li>
-                <li>
-                    <NavLink to ="/admin" activeClassName="active">
-                        ADMIN
+                    </li>
+                    <li>
+                        <NavLink to="/admin" activeClassName=" active" className="nav-link">
+                            ADMIN
                     </NavLink>
-                </li>
-            </ul>
-        </div>
-        <div className="col-3 text-right">
-            login
-        </div>
-    </nav>
-)
+                    </li>
+                </ul>
+            </div>
+            <div className="col-3 text-right">
+                {currentUser && (
+                <>
+                {currentUser}
+                    <a href="#logout" className="nav-link active d-inline">
+                    LOGOUT
+                    </a>
+                </>
+                )}
+            {!currentUser &&(
+                 <Link to ="/auth/login" className="nav-link active">
+                 LOGIN
+             </Link>
+            )}
+            </div>
+        </nav>
+    );
+}
 
 export default Navbar;
