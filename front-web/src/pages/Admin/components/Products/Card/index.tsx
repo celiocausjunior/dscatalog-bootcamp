8e2caccf6a2cc999999060167af2b44fd9f0314a
Celio Caus Junior <celiocausjunior@gmail.com>
2020-12-20 00:45:27 -0300

    Creating Product Cards Admin

---
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -0,0 +1,29 @@
+import ProductPrice from 'pages/Catalog/components/ProductPrice';
+import React from 'react';
+import './styles.scss'
+
+const Card = () => {
+    return (
+        <div className="card-base product-card-admin">
+            <div className="row">
+                <div className="col-2 text-center border-right py-3" >
+                    <img
+                        src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
+                        alt="Produto"
+                        className="product-card-image-admin"
+                    />
+                </div>
+                <div className="col-7">
+                    <h3 className="product-card-name-admin py-3"> Computador I7</h3>
+                    <ProductPrice price={40.5} />
+                </div>
+                <div className="col-3 pt-3 pr-5">
+                    <button type="button" className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit">EDITAR</button>
+                    <button type="button" className="btn btn-outline-danger btn-block border-radius-10 ">EXCLUIR</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
