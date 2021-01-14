25787373cfaece450e06735eec4b069551146181
Celio Caus Junior <celiocausjunior@gmail.com>
2021-01-14 16:58:38 -0300

    Category Admin Form 5-26

--- front-web/src/pages/Admin/components/Categories/Form/index.tsx
+++ front-web/src/pages/Admin/components/Categories/Form/index.tsx
@@ -1,20 +1,47 @@
-import React, { Children } from 'react';
+import { makePrivateRequest } from 'core/utils/request';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import BaseForm from '../../BaseForm';
 
 type FormState = {
-    name?: string;
+    category: string;
 }
 
 const Form = () => {
-    return (
-        <BaseForm title="CADASTRAR UMA CATEGORIA">
-            <div className="row">
-                <div className="col-6">
-                    <input type="text" className="form-control" />
-                </div>
-            </div>
+    const { register, handleSubmit, errors } = useForm<FormState>();
 
-        </BaseForm>
+    const onSubmit = (formData: FormState) => {
+        console.log(formData);
+        makePrivateRequest({ url: '/categories', method: 'POST', data: formData })
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <BaseForm title="CADASTRAR UMA CATEGORIA">
+                <div className="row">
+                    <div className="col-6">
+                        <div>
+                            <input
+                                ref={register({ 
+                                    required: "Campo obrigatório",
+                                    minLength: { value: 5, message:"O campo deve ter no mínimo 5 caracteres"},
+                                    maxLength: { value: 5, message:"O campo deve ter no máximo 60 caracteres"},
+                                })}
+                                name="category"
+                                type="text"
+                                className="form-control"
+                                placeholder="Nome da categoria"
+                            />
+                            {errors.category && (
+                                <div className="invalid-feedback d-block">
+                                    {errors.category.message}
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </BaseForm>
+        </form>
     );
 }
 
